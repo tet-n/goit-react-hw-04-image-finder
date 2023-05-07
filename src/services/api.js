75268e23063946238fa7dda58842170687eede04
Tetiana Nivchyk <tata.02od@gmail.com>
2023-05-07 22:09:20 +0300

    added try-catch

--- src/services/api.js
+++ src/services/api.js
@@ -3,22 +3,26 @@ const BASEURL = 'https://pixabay.com/api/';
 const KEY = '14920021-2257a961c5f1892eae42399ba';
 
 export const fetchImages = async (name, page = 1) => {
-  const { data } = await axios({
-    url: BASEURL,
-    method: 'get',
-    params: {
-      key: KEY,
-      q: name,
-      image_type: 'photo',
-      orientation: 'horizontal',
-      page,
-      per_page: 12,
-      safesearch: true,
-      headers: {
-        'Content-Type': 'application/json',
+  try {
+    const { data } = await axios({
+      url: BASEURL,
+      method: 'get',
+      params: {
+        key: KEY,
+        q: name,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        page,
+        per_page: 12,
+        safesearch: true,
+        headers: {
+          'Content-Type': 'application/json',
+        },
       },
-    },
-  });
+    });
 
-  return data;
+    return data;
+  } catch (error) {
+    console.error(error.message);
+  }
 };
